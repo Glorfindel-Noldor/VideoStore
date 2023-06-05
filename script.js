44f41b222e162dde16723a116c23de5113f43dc9
Alex Rios <alexrios.dev@gmail.com>
2023-06-04 21:52:41 -0500

    updating script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-const h2 = document.createElement("h2");
-h2.textContent = "this content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// Waits until the DOM is completely rendered, then fires up JS code
+document.addEventListener('DOMContentLoaded', async () => {
+  let coffees = await fetchCoffees();
+  renderCoffees(coffees);
+});
+
+const COFFEE_API = 'https://api.sampleapis.com/coffee/hot';
+
+const fetchCoffees = async () => {
+  let response = await fetch(COFFEE_API);
+  let data = await response.json();
+  return data;
+};
+
+const renderCoffees = (coffees) => {
+  let parentNode = document.querySelector('#display-data');
+
+  coffees.forEach((coffee) => {
+    let titleNode = document.createElement('h2');
+    titleNode.innerText = coffee.title;
+
+    let descNode = document.createElement('p');
+    descNode.innerText = coffee.description;
+
+    let ingrNode = document.createElement('p');
+    ingrNode.innerText = `Ingredients: ${coffee.ingredients.join(', ')}`;
+
+    let childNode = document.createElement('div');
+    childNode.appendChild(titleNode);
+    childNode.appendChild(descNode);
+    childNode.appendChild(ingrNode);
+
+    parentNode.appendChild(childNode);
+  });
+};
